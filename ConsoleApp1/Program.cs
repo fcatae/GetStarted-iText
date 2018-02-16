45ff58859eb69575034e003ab247e5c5811f6b43
Fabricio Catae <fcatae@microsoft.com>
2018-02-16 17:45:58 -0200

    add a simple Read

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
 
 namespace ConsoleApp1
 {
@@ -7,6 +10,26 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string filename = "samples/p40.pdf";
+
+            string simpleResult = ReadTextFromPdf(filename);
+
+            Console.WriteLine("ReadTextFromPdf");
+            Console.WriteLine("============================================================");
+            Console.WriteLine(simpleResult.Substring(0, 1000));
+            Console.WriteLine();            
         }
+
+        static string ReadTextFromPdf(string filename)
+        {
+            using (var pdf = new PdfDocument(new PdfReader(filename)))
+            {
+                var page = pdf.GetFirstPage();
+
+                return PdfTextExtractor.GetTextFromPage(page);
+            }
+        }       
+
     }
 }
